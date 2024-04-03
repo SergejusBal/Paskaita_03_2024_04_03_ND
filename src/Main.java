b5ee42e3ed8a9152ac3d5079b8a41343a7fb420e
Sergejus <Sergejus.balciunas@gmail.com>
2024-04-03 23:11:42 +0300

    ND

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,120 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        ////////////////////////////////////////////
+        ////Pirma uzduotis
+
+        System.out.println();
+        System.out.println("Pirma uzduotis");
+        System.out.println();
+
+        Random random = new Random();
+        byte[] masyvas8 = new byte[8];
+
+        for(int i = 0; i< masyvas8.length;i++){
+            masyvas8[i] = (byte) random.nextInt(1,10);
         }
+
+        for(int i = 0; i < masyvas8.length;i++){
+           System.out.println("Verte prie indekso " + i +" yra: " +masyvas8[i]);
+       }
+
+        ////////////////////////////////////////////
+        ////Antra uzduotis
+
+        System.out.println();
+        System.out.println("Antra uzduotis");
+        System.out.println();
+
+
+        System.out.println("Vertes didesnės uz 4 yra");
+        for (byte b:masyvas8){
+            if(b>4) System.out.println(b);
+        }
+
+
+        byte[] naujasMasyvas = new byte[8];
+        for(int j = 0, i = 0; i < masyvas8.length;i++){
+            if(masyvas8[i]<=4) {
+                naujasMasyvas[j] = masyvas8[i];
+                j++;
+            }
+        }
+        System.out.println("Vertes mazesnes arba lygios 4 yra");
+        for (byte b:naujasMasyvas){
+            System.out.println(b);
+        }
+
+
+        int suma = 0;
+        for(int i = 0; i< masyvas8.length;i++)        {
+           suma+=masyvas8[i];
+        }
+
+        System.out.println("Suma yra: "+ suma);
+
+        ////////////////////////////////////////////
+        ////Trecia uzduotis
+
+        System.out.println();
+        System.out.println("Trecia uzduotis");
+        System.out.println();
+
+        List<Integer> randomVertes = new ArrayList<Integer>();
+
+        for(int i = 0; i<8;i++){
+            randomVertes.add(random.nextInt(0,10));
+        }
+        System.out.println(randomVertes);
+
+
+
+        List<Integer> randomDaUz4 = new ArrayList<Integer>();
+        for(Integer i:randomVertes){
+            if(i>4) randomDaUz4.add(i);
+
+        }
+        System.out.println(randomDaUz4);
+
+
+
+        List<Integer> randomMaUz4 = new ArrayList<Integer>();
+        for(Integer i:randomVertes){
+            if(i<=4) randomMaUz4.add(i);
+        }
+        System.out.println(randomMaUz4);
+
+
+        suma = 0;
+        for(Integer i:randomVertes){
+            suma+=i;
+        }
+
+        System.out.println("Suma yra: " + suma);
+        System.out.println("Vidurkis yra: " + (float) suma/randomVertes.size());
+
+        System.out.println("suma yra: " + randomVertes.stream().mapToInt(i -> i).sum());
+        System.out.println("vidurkis yra: " + randomVertes.stream().mapToInt(i -> i).average().getAsDouble());
+
+
+        ////////////////////////////////////////////
+        ////Ketvirta uzduotis
+
+        System.out.println();
+        System.out.println("Ketvirta uzduotis");
+        System.out.println();
+
+        ///Ketvirta uzduotis neikelta, tik atsakymas ikeltas i rar.
+
+
+
     }
 }
